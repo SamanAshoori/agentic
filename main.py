4c33a07eec14c947a62c0cce7fb37ab6a3d84055
Saman <saman.ashoori@outlook.com>
2026-02-26 23:07:06 +0000

    eda_agent completed

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pandas as pd
-from crewai import Agent,Task,Crew
+import os
+from crewai import LLM, Agent, Crew, Task
 from langchain_community.llms import ollama
 
 #Load training data
-data = pd.read_csv('training_data.csv')
+data = pd.read_csv('fraudTrain.csv')
 
 #create quick summary of data for the agent
 summary = data.describe().to_string()
 data_head = data.head().to_markdown()
 
+prompt = summary + data_head
+print(f"Approx token count: {len(prompt.split()) * 1.3:.0f}")
+
 #local llm
-llm = ollama.Ollama(model = "llama3.2")
+llm = LLM(
+    model="ollama/llama3.2",
+    base_url="http://localhost:11434"
+)
 
 #define agent params
 eda_agent = Agent(
+    name = "EDA Agent",
     role = "Data Analyst",
     goal="Analyse the raw credit card dataset and identify key features,target variables and potential data quality issues",
-    backstory = "You are a data analyst tasked with analyzing a raw credit card dataset in prepartation for logistic regression. " \
-    "You communicate clearly and in a concise manner. " \
-    "You have access to a summary of the dataset and the first few rows of the data to assist you in your analysis." 
+    backstory = "You are a data analyst tasked with analyzing a raw credit card dataset in prepartation for logistic regression. You communicate clearly and in a concise manner. You have access to a summary of the dataset and the first few rows of the data to assist you in your analysis.",
     verbose = True,
     allow_delegation = False,
     llm = llm
@@ -45,8 +51,7 @@ eda_task = Task(
 
 if __name__ == "__main__":
     #create crew and add task
-    crew = Crew()
-    crew.add_task(eda_task)
+    crew = Crew(agents=[eda_agent], tasks=[eda_task], verbose=True)
 
     print("Starting the crew to analyze the dataset...")
     #run the crew
